9b472c2ddde2d434a28fcfd25de4f4b26b1de606
Josh Cheek <josh.cheek@gmail.com>
2019-01-02 07:35:11 -0600

    Copy embedded transducer tests into the test suite

--- spec/transducer_spec.rb
+++ spec/transducer_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe 'transducers' do
       expect(maps_result).to eq([2,4,6])
     end
   end
+
+  context 'acceptance' do
+    example 'mapping and filtering' do
+      result = [1,2,3,4,5]
+        .reduce([], &mapping   { |v| v * 3   }.call(concats))
+        .reduce([], &filtering { |v| v.even? }.call(concats))
+      expect(result).to eq [6, 12]
+    end
+
+    example 'works with Ramda' do
+      # `gem install ramda-ruby`
+      require 'ramda'
+      result = [1,2,3,4,5].reduce([], &Ramda.pipe(
+        mapping   { |v| v + 1   },
+        filtering { |v| v.even? },
+        mapping   { |v| v * 3   }
+      ).call(concats))
+      expect(result).to eq [7, 13]
+    end
+  end
 end
